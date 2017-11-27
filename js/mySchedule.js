387ecb2c83ae96a51d894990da3b769f91280924
Mayank Bansal <sunny.bansal@gmail.com>
2017-11-27 07:49:24 -0600

    Schedule Viewer

--- js/mySchedule.js
+++ js/mySchedule.js
@@ -177,39 +177,359 @@ myScheduleApp.angular.controller('homeController', function ($scope, $http) {
     $scope.init();
 });
 
-
-myScheduleApp.angular.controller('scheduleController', function ($scope, $http) {
+myScheduleApp.angular.controller('scheduleController', function ($scope, store, $http) {
     $scope.init = function () {
         myScheduleApp.currentPage = myScheduleApp.pages.schedules;
+
+        $scope.mySchedules = [];
+
+        var myUser = store.get('userObject');
+        if (myUser) $scope.user = myUser;
+
+        $scope.getSchedules();
+    };
+
+    $scope.getSchedules = function () {
+        var schedulesRef = firebase.database().ref().child("schedules");
+        schedulesRef.once('value', function (snap) {
+            $scope.safeApply(function () {
+                snap.forEach(function (childSnap) {
+                    var schedule = childSnap.val();
+                    if ($scope.user.token === schedule.userToken)
+                        $scope.mySchedules.push(schedule);
+                });
+            });
+        });
     };
 
     $scope.init();
 });
 
+function clean(variable) {
+    return JSON.parse(angular.toJson(variable));
+}
 
-myScheduleApp.angular.controller('newScheduleController', function ($scope, $http) {
-
+myScheduleApp.angular.controller('newScheduleController', function ($scope, store, $window, $http) {
     $scope.init = function () {
+
+
+        $scope.schedulefirebaseToken = null;
+
+        var myUser = store.get('userObject');
+        if (myUser) {
+            $scope.user = myUser;
+        }
+
         $scope.currentSchedule = {
             name: "Untitled Schedule",
             credits: 0,
-            timeSaved: "Not Saved"
+            timeSaved: "Not Saved",
+            userToken: $scope.user.token,
+            sunday: [],
+            monday: [
+                {
+                    title: "Introduction to Algorithms",
+                    subject: "CS",
+                    course: 430,
+                    CRN: 20047,
+                    section: "01",
+                    location: "MC",
+                    credits: 3,
+                    timeSlot: [2],
+                    classType: "normal",
+                    requires: 28173,
+                    professor: "Edward M Reingold"
+                },
+                {
+                    title: "Technical Communication",
+                    subject: "COM",
+                    course: 421,
+                    CRN: 21008,
+                    section: "01",
+                    location: "MC",
+                    credits: 3,
+                    timeSlot: [3],
+                    classType: "normal",
+                    requires: null,
+                    professor: "Andrew J Roback"
+                },
+                {
+                    title: "Computer Organization & Assembler Programming",
+                    subject: "CS",
+                    course: 350,
+                    CRN: 20033,
+                    section: "01",
+                    location: "MC",
+                    credits: 3,
+                    timeSlot: [4],
+                    classType: "normal",
+                    requires: 22642,
+                    professor: "Kyle C Hale"
+                }
+            ],
+            tuesday: [
+                {
+                    title: "Data Mining",
+                    subject: "CS",
+                    course: 422,
+                    CRN: 28859,
+                    section: "01",
+                    location: "MC",
+                    credits: 3,
+                    timeSlot: [6, 7],
+                    classType: "normal",
+                    requires: null,
+                    professor: "Vijay Kumar Gurbani"
+                }
+            ],
+            wednesday: [
+                {
+                    title: "Introduction to Algorithms",
+                    subject: "CS",
+                    course: 430,
+                    CRN: 20047,
+                    section: "01",
+                    location: "MC",
+                    credits: 3,
+                    timeSlot: [2],
+                    classType: "normal",
+                    requires: 28173,
+                    professor: "Edward M Reingold"
+                },
+                {
+                    title: "Technical Communication",
+                    subject: "COM",
+                    course: 421,
+                    CRN: 21008,
+                    section: "01",
+                    location: "MC",
+                    credits: 3,
+                    timeSlot: [3],
+                    classType: "normal",
+                    requires: null,
+                    professor: "Andrew J Roback"
+                },
+                {
+                    title: "Computer Organization & Assembler Programming",
+                    subject: "CS",
+                    course: 350,
+                    CRN: 20033,
+                    section: "01",
+                    location: "MC",
+                    credits: 3,
+                    timeSlot: [4],
+                    classType: "normal",
+                    requires: 22642,
+                    professor: "Kyle C Hale"
+                }
+            ],
+            thursday: [
+                {
+                    title: "Database Organization",
+                    subject: "CS",
+                    course: 425,
+                    CRN: 20044,
+                    section: "01",
+                    location: "MC",
+                    credits: 3,
+                    timeSlot: [6, 7],
+                    classType: "normal",
+                    requires: null,
+                    professor: "Omar Aldawud"
+                }
+            ],
+            friday: [
+                {
+                    title: "(Recitation) Introduction to Algorithms",
+                    subject: "CS",
+                    course: 430,
+                    CRN: 28173,
+                    section: "R01",
+                    location: "MC",
+                    credits: 0,
+                    timeSlot: [2],
+                    classType: "shortened",
+                    requires: 20047,
+                    professor: "Edward M Reingold"
+                }
+            ],
+            saturday: []
         };
+
+        $scope.colors = [
+            {background: "#EE534F", color: "#FFFFFF"},
+            {background: "#FFA827", color: "#FFFFFF"},
+            {background: "#9DCC64", color: "#FFFFFF"},
+            {background: "#AA47BC", color: "#FFFFFF"},
+            {background: "#5C6AC0", color: "#FFFFFF"},
+            {background: "#29B6F6", color: "#FFFFFF"},
+            {background: "#FFEE58", color: "#000000"},
+        ];
+
+        $scope.timeSlots = [
+            {start: "08:35 AM", end: "09:50 AM"},
+            {start: "10:00 AM", end: "11:15 AM"},
+            {start: "11:15 AM", end: "12:40 PM"},
+            {start: "01:50 PM", end: "03:05 PM"},
+            {start: "03:15 PM", end: "04:30 PM"},
+            {start: "05:00 PM", end: "06:15 PM"},
+            {start: "06:25 PM", end: "07:40 AM"},
+            {start: "07:50 PM", end: "09:05 PM"}
+        ];
+
         myScheduleApp.currentPage = myScheduleApp.pages.newSchedule;
+
+        $scope.processSchedule();
+    };
+
+    $scope.iterateTimes = function (day, classes) {
+
+        // INITIALIZE ALL SLOTS AS EMPTY
+        var slots = [0, 1, 2, 3, 4, 5, 6, 7];
+
+        // FIND TIMES OF CLASSES
+        for (var i = 0; i < 7; i++) {
+            if (day[i]) {
+                day[i].times = $scope.getTimeSlot(day[i]);
+                var classCode = day[i].subject + " " + day[i].course;
+                if (classes.indexOf(classCode) === -1)
+                    classes.push(classCode);
+
+                // INIT AS NOT SPACER
+                day[i].spacer = false;
+
+                // INIT BOX SIZE
+                if (day[i].classType === "shortened")
+                    day[i].boxSize = "smallBox";
+                else if (day[i].timeSlot.length === 1)
+                    day[i].boxSize = "normalBox";
+                else if (day[i].timeSlot.length > 1)
+                    day[i].boxSize = "x" + day[i].timeSlot.length + "Box";
+
+                // MARK SLOT AS FULL | REMOVE FROM SLOTS ARRAY
+                for (var j = 0; j < day[i].timeSlot.length; j++) {
+                    var index = slots.indexOf(day[i].timeSlot[j]);
+                    slots.splice(index, 1);
+                }
+            }
+        }
+
+        // ADD EMPTY SLOTS
+        for (var i = 0; i < 7; i++) {
+            if (slots.indexOf(i) !== -1) {
+                var emptyClass = {
+                    spacer: true,
+                    timeSlot: [i]
+                };
+
+                day.push(emptyClass);
+            }
+        }
+
+        // SORT ACCORDING TO TIMESLOT POSITION
+        for (var i = 0; i < 7 - 1; i++) {
+            for (var j = 0; j < 7 - i - 1; j++) {
+                var timeSlotCompare1, timeSlotCompare2;
+
+                if (day[j].timeSlot.length > 1) timeSlotCompare1 = day[j].timeSlot[0];
+                else timeSlotCompare1 = day[j].timeSlot;
+
+                if (day[j + 1].timeSlot.length > 1) timeSlotCompare2 = day[j + 1].timeSlot[0];
+                else timeSlotCompare2 = day[j + 1].timeSlot;
+
+                if (timeSlotCompare1 > timeSlotCompare2) {
+                    var temp = day[j];
+                    day[j] = day[j + 1];
+                    day[j + 1] = temp;
+                }
+            }
+        }
+
+        console.log("Empty Slots: ", slots);
+    };
+
+    $scope.iterateColors = function (day) {
+        for (var i = 0; i < day.length; i++) {
+            if (day[i].title) {
+                var classCode = day[i].subject + " " + day[i].course;
+                day[i].color = $scope.colors[$scope.myClasses.indexOf(classCode)];
+            }
+        }
+    };
+
+    $scope.saveSchedule = function () {
+        $scope.currentSchedule.timeSaved = new Date();
+        var schedulesRef = firebase.database().ref().child("schedules");
+
+        schedulesRef.child($scope.schedulefirebaseToken + "").once('value', function (snap) {
+            if (snap.exists()) {
+                console.log("Schedule Updated");
+                schedulesRef.child($scope.schedulefirebaseToken + "").update(clean(($scope.currentSchedule)));
+            } else {
+                $scope.schedulefirebaseToken = (schedulesRef.push(clean($scope.currentSchedule))).key;
+                console.log("Schedule Added");
+            }
+        });
+    };
+
+    $scope.deleteSchedule = function () {
+        var schedulesRef = firebase.database().ref().child("schedules");
+        schedulesRef.child($scope.schedulefirebaseToken + "").remove();
+        console.log("Schedule Removed");
+        $scope.currentSchedule.timeSaved = "Not Saved";
+        $scope.go("/schedules");
+    };
+
+    $scope.processSchedule = function () {
+
+        $scope.myClasses = [];
+
+        $scope.iterateTimes($scope.currentSchedule.sunday, $scope.myClasses);
+        $scope.iterateTimes($scope.currentSchedule.monday, $scope.myClasses);
+        $scope.iterateTimes($scope.currentSchedule.tuesday, $scope.myClasses);
+        $scope.iterateTimes($scope.currentSchedule.wednesday, $scope.myClasses);
+        $scope.iterateTimes($scope.currentSchedule.thursday, $scope.myClasses);
+        $scope.iterateTimes($scope.currentSchedule.friday, $scope.myClasses);
+        $scope.iterateTimes($scope.currentSchedule.saturday, $scope.myClasses);
+
+        $scope.iterateColors($scope.currentSchedule.sunday, $scope.myClasses);
+        $scope.iterateColors($scope.currentSchedule.monday, $scope.myClasses);
+        $scope.iterateColors($scope.currentSchedule.tuesday, $scope.myClasses);
+        $scope.iterateColors($scope.currentSchedule.wednesday, $scope.myClasses);
+        $scope.iterateColors($scope.currentSchedule.thursday, $scope.myClasses);
+        $scope.iterateColors($scope.currentSchedule.friday, $scope.myClasses);
+        $scope.iterateColors($scope.currentSchedule.saturday, $scope.myClasses);
+
+        console.log($scope.myClasses);
+    };
+
+    $scope.getTimeSlot = function (myClass) {
+        var classTime = "";
+
+        var startTime = $scope.timeSlots[myClass.timeSlot[0]] + " ";
+
+        var classStartPieces = startTime.split(" ");
+        var classStartPiecesTime = classStartPieces[0].split(":");
+        var shortenedEnd = (Math.floor(classStartPiecesTime[0] + 5 / 6));
+
+        if (shortenedEnd > 12)
+            shortenedEnd -= 12, classStartPieces[1] = "PM";
+        shortenedEnd = shortenedEnd + ":" + (classStartPieces[1] + 50) % 60 + " " + classStartPieces[1];
+
+
+        if (myClass.classType === "shortened")
+            classTime = $scope.timeSlots[myClass.timeSlot[0]].start + " - " + shortenedEnd;
+        if (myClass.timeSlot.length > 1)
+            classTime = $scope.timeSlots[myClass.timeSlot[0]].start + " - " + $scope.timeSlots[myClass.timeSlot[myClass.timeSlot.length - 1]].end;
+        else if (myClass.timeSlot.length === 1)
+            return $scope.timeSlots[myClass.timeSlot[0]].start + " - " + $scope.timeSlots[myClass.timeSlot[0]].end;
+        else
+            return "Time Error";
+
+        return classTime;
     };
 
     $scope.init();
-
-    $scope.schedule = {
-        sunday: [],
-        monday: [],
-        tuesday: [],
-        wednesday: [],
-        thursday: [],
-        friday: [],
-        saturday: []
-    }
-
 });
 
 myScheduleApp.angular.controller('dashboardController', function (store, $scope, $http) {
